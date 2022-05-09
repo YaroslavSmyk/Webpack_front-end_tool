effde5862cb424fb56f3392724770d67caf8e684
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-05-09 11:49:45 +0300

    test

--- lesson1/task1/src/scripts/logger/logger.js
+++ lesson1/task1/src/scripts/logger/logger.js
@@ -1,17 +1,15 @@
-export const createLogger = name  => {
-    const logs = [];
+export const createLogger = (name) => {
+  const logs = [];
 
-    return {
-        log(message) {
-logs.push(`log - ${name} - ${message}`);
-        },
-        error(errorText) {
-            logs.push(`error - ${name} - ${errorText}`);
-
-        },
-        getLogs() {
-            return logs;
-        }
-    };
+  return {
+    log(message) {
+      logs.push(`log - ${name} - ${message}`);
+    },
+    error(errorText) {
+      logs.push(`error - ${name} - ${errorText}`);
+    },
+    getLogs() {
+      return logs;
+    },
+  };
 };
-
