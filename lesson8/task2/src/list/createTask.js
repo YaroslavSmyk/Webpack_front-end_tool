4d455143a1ce66125f4766e79634b7f0d58eba04
Yaroslav Smyk <yaroslavsmyk@gmail.com>
2022-05-12 12:23:57 +0300

    v2

---
+++ lesson8/task2/src/list/createTask.js
@@ -0,0 +1,28 @@
+import { renderTasks } from './renderer.js';
+import { getItem, setItem } from './storage.js';
+import { createTask, getTasksList } from './tasksGateway.js';
+
+export const onCreateTask = () => {
+  const inputTaskElem = document.querySelector('.task-input');
+
+  const text = inputTaskElem.value;
+
+  if (!text) {
+    return;
+  }
+
+  inputTaskElem.value = '';
+
+  const newTask = {
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+  };
+
+  createTask(newTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      setItem('tasksList', newTasksList);
+      renderTasks();
+    });
+};
